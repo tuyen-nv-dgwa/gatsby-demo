9dade75f02e60d2fa1e50253864a7a6b40e68260
tuyennv <nguyenvantuyenx2bk@gmail.com>
2023-12-26 10:31:36 +0700

    test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-     <p>I'm making this by following the Gatsby Tutorial.</p>
+     <p>I'm making this by following the Gatsby Tutorial.test test</p>
     </Layout>
   )
 }
